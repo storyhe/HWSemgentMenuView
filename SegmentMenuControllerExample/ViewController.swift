026786aefbf17a517002a5853b9de37712ad0d60
Hyeonwoo Park <storyhe.love@gmail.com>
2020-08-01 13:52:22 +0900

    Initial Commit

---
+++ SegmentMenuControllerExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SegmentMenuControllerExample
+//
+//  Created by Hyeonwoo Park on 2020/08/01.
+//  Copyright © 2020 muzc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
